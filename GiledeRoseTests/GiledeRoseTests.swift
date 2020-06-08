eb50c432dfbb23ba40ab99859ad7f52b10343e25
Reece Liu <yankailiu@hipagesgroup.com.au>
2020-06-08 16:50:09 +0800

    Initial Commit

---
+++ GiledeRoseTests/GiledeRoseTests.swift
@@ -0,0 +1,34 @@
+//
+//  GiledeRoseTests.swift
+//  GiledeRoseTests
+//
+//  Created by Yankai Liu on 2020/6/8.
+//  Copyright © 2020 ThoughtWorks. All rights reserved.
+//
+
+import XCTest
+@testable import GiledeRose
+
+class GiledeRoseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
