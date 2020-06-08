75ddd7c28f2ec34e522f5df85f51cbfefb6116fa
Reece Liu <yankailiu@hipagesgroup.com.au>
2020-06-08 17:05:07 +0800

    create product model

---
+++ GiledeRose/GiledeRose/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  GiledeRose
+//
+//  Created by Yankai Liu on 2020/6/8.
+//  Copyright © 2020 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class Product {
+    var sellIn: Int
+    var quality: Int
+    
+    init(sellIn: Int, quality: Int) {
+        self.sellIn = sellIn
+        self.quality = quality
+    }
+}
+
+
